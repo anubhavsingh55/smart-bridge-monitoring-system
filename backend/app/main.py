c6a75ec7979cc905426c15089b95f5b1ffcae234
Anubhav Singh <s24cseu1285@bennett.edu.in>
2026-06-03 12:33:47 +0530

    Fix Vercel CORS origin

--- backend/app/main.py
+++ backend/app/main.py
@@ -11,7 +11,8 @@ app.add_middleware(
     CORSMiddleware,
     allow_origins=[
         "http://localhost:5173",
-        "http://127.0.0.1:5173"
+        "http://127.0.0.1:5173",
+        "https://smart-bridge-monitoring-system-jx785iq49.vercel.app"
     ],
     allow_credentials=True,
     allow_methods=["*"],
